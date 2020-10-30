60b29b4a37b4fd2ae729f0c12354d740d944a7eb
a01745043 <bruno_estrada@DESKTOP-N1JJTVP.localdomain>
2020-10-29 22:44:52 -0600

    se agregaron nuevas cosas

--- convolucion2.py
+++ convolucion2.py
@@ -52,9 +52,9 @@ def HEEHEE (A):
     B = np.zeros((len(A),len(A[0])))
     for i in range(len(A)):
         for j in range(len(A[0])):
-            if A[i][j]<100:
+            if A[i][j]<50:
                 B[i][j] = 0
-            elif A[i][j] > 100:
+            elif A[i][j] > 50:
                 B[i][j] = 255
     return B
                 
